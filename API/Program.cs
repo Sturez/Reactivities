f5b6994fcb41e2409f7dcddff6337ddcb4bc1613
Sturez <sturez@msn.com>
2023-01-20 12:45:34 +0100

    setup react app that's using the api, and added some ui

--- API/Program.cs
+++ API/Program.cs
@@ -15,6 +15,17 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("ReactApp_CorsPolicy", policy =>
+    {
+        policy.AllowAnyMethod()
+              .AllowAnyHeader()
+              .WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("ReactApp_CorsPolicy");
 
 app.UseAuthorization();
 
