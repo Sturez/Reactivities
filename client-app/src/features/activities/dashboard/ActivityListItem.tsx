d93ae6bc14b6b67374fd6f9c176f323ea7042fbc
Sturez <sturez@msn.com>
2023-02-02 10:04:57 +0100

    styled activity list and single activity

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import React, { SyntheticEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Item, Button, Label } from 'semantic-ui-react';
+import { Item, Button, Label, Segment, Icon } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/store/store';
 
@@ -13,37 +13,41 @@ const ActivityListItem = ({ activity }: Props) => {
     const [target, setTarget] = useState<string>('');
     const { activityStore } = useStore();
     const { loading, deleteActivity } = activityStore;
-    
+
     function handleActivityDelete(evt: SyntheticEvent<HTMLButtonElement>, id: string) {
         setTarget(evt.currentTarget.name);
         deleteActivity(id);
     }
- 
+
     return (
-        <Item key={activity.id}>
-            <Item.Content>
-                <Item.Header>{activity.title}</Item.Header>
-                <Item.Meta>{activity.date}</Item.Meta>
-                <Item.Description>
-                    <div>{activity.description}</div>
-                    <div>{activity.city},{activity.venue}</div>
-                </Item.Description>
-                <Item.Extra>
-                    <Button floated="right" content="View" color="blue"
-                        as={Link} to={`/activities/${activity.id}`} />
+        <Segment.Group key={activity.id}>
+            <Segment>
+                <Item.Group>
+                    <Item.Image size='tiny' circular src='/assets/user.png' />
+                    <Item.Content>
+                        <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                            {activity.title}
+                        </Item.Header>
+                        <Item.Description> Hosted By Franco</Item.Description>
+                    </Item.Content>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span><Icon name='clock' />{activity.date}</span>
+                <span><Icon name='marker' />{activity.venue}</span>
+            </Segment>
+            <Segment secondary>
+                Attendees goes here
+            </Segment>
+            <Segment clearing>
+                <span>
+                    {activity.description}
+                    <Button as={Link} to={`/activities/${activity.id}`}
+                        color='teal' floated='right' content='View' />
+                </span>
+            </Segment>
 
-                    <Button
-                        name={activity.id}
-                        floated="right"
-                        content="Delete"
-                        color="red"
-                        loading={loading && target === activity.id}
-                        onClick={(evt) => handleActivityDelete(evt, activity.id)} />
-
-                    <Label content={activity.category} />
-                </Item.Extra>
-            </Item.Content>
-        </Item>
+        </Segment.Group>
     );
 };
 
