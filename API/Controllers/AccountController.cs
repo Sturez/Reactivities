b2b5cc4686a00ad1719450c2a39f67efdf72110a
Sturez <sturez@msn.com>
2023-02-06 19:45:03 +0100

    Creating new users

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -40,6 +41,41 @@ namespace API.Controllers
                 Image = null,
                 Token = _tokenService.CreateToken(user)
             };
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await _userManager.Users.AnyAsync(u => u.UserName == registerDto.Username))
+            {
+                return BadRequest($"Username {registerDto.Username} is already taken");
+            }
+
+            if (await _userManager.Users.AnyAsync(u => u.Email == registerDto.Email))
+            {
+                return BadRequest($"A user with that email is already redistered");
+            }
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username,
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email
+            };
+
+            var createdUser = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (createdUser.Succeeded)
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = _tokenService.CreateToken(user),
+                    Username = user.UserName,
+                };
+
+
+            return BadRequest(createdUser.Errors);
         }
 
     }
